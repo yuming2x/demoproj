c7eaec4b65c0b90162c8280fa59241cb5ced4148
YumingZhang <yumingx.zhang@intel.com>
2018-09-26 13:51:26 +0800

    version 1.0 @dev1

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+VERSION = '1.0'
+
+def printVersion(v):
+    print 'Version: {}'.format(v)
+
+def main():
+    printVersion(VERSION)
+
+if __name__ == '__main__':
+    main()
